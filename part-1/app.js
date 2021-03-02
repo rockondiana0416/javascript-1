11b70587351dfebd2fb02e04aeb881aa602f5746
Diana Hinchcliffe <rockondiana0416@yahoo.com>
2021-03-02 14:15:46 -0800

    Changed console.log

--- part-1/app.js
+++ part-1/app.js
@@ -1,7 +1,7 @@
 // Log to console
 console.log('Hello World');
 console.log('123');
-// console.log('true');
+console.log('true');
 // var greeting = 'Hello';
 // console.log(greeting);
 // console.log([1,2,3,4]);
