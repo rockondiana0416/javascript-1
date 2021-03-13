524b46d66556e3aea34cf7cfa7b95c9094df068b
Diana Hinchcliffe <rockondiana0416@yahoo.com>
2021-03-12 18:51:13 -0800

    Initial commit of app.js

---
+++ task-list/app.js
@@ -0,0 +1,4 @@
+// Define UI Vars
+const form = document.querySelector('#tast-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
